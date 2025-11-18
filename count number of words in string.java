bc4cd3c1d7155349446779eab1cf4571f23af3bc
sujeetkumaryadav <61322429+sujeetkumaryadav@users.noreply.github.com>
2025-11-18 11:37:04 +0530

    Create count number of words in string.java

---
+++ count number of words in string.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Concat{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter sring");
+String str1=sc.nextLine();
+String[] res=str1.split(" ");
+System.out.println(res.length);
+
+}
+}
